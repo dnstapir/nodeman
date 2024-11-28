634f7ab762ea4ac8005a6f1c11b4885d8ddcf8c1
Jakob Schlyter <jakob@kirei.se>
2024-11-28 08:46:35 +0100

    more CSR testing

--- nodeman/utils.py
+++ nodeman/utils.py
@@ -1,13 +1,14 @@
 from cryptography import x509
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import ec
-from cryptography.x509.oid import NameOID
+from cryptography.x509.oid import ExtensionOID, NameOID
 from jwcrypto.jwk import JWK
 
 type PrivateKey = ec.EllipticCurvePrivateKey
 
 
 def generate_x509_csr(name: str, key: PrivateKey) -> x509.CertificateSigningRequest:
+    """Generate X.509 CSR with name and key"""
     return (
         x509.CertificateSigningRequestBuilder()
         .subject_name(x509.Name([x509.NameAttribute(NameOID.COMMON_NAME, name)]))
@@ -17,6 +18,30 @@ def generate_x509_csr(name: str, key: PrivateKey) -> x509.CertificateSigningRequ
         )
         .sign(key, hashes.SHA256())
     )
+
+
+def verify_x509_csr(name: str, csr: x509.CertificateSigningRequest) -> None:
+    """Verify X.509 CSR against name"""
+
+    # ensure Subject is correct
+    if len(csr.subject) != 1:
+        raise ValueError("Invalid Subject")
+    cn = csr.subject.get_attributes_for_oid(NameOID.COMMON_NAME)
+    if len(cn) == 0:
+        raise ValueError("Missing CommonName")
+    elif len(cn) > 1:
+        raise ValueError("Multiple CommonName")
+    elif cn[0].value != name:
+        raise ValueError("Invalid CommonName")
+
+    # ensure we only have a single extension
+    if len(csr.extensions) != 1:
+        raise ValueError("Multiple extensions")
+
+    # ensure SubjectAlternativeName is correct
+    ext = csr.extensions.get_extension_for_oid(ExtensionOID.SUBJECT_ALTERNATIVE_NAME)
+    if ext.value.get_values_for_type(x509.DNSName) != [name]:
+        raise ValueError("Invalid SubjectAlternativeName")
 
 
 def jwk_to_alg(key: JWK) -> str:
---
+++ tests/test_x509.py
@@ -0,0 +1,55 @@
+import pytest
+from cryptography import x509
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.x509.oid import NameOID
+
+from nodeman.utils import verify_x509_csr
+
+type PrivateKey = ec.EllipticCurvePrivateKey
+
+NAME_1 = "host1.example.com"
+NAME_2 = "host2.example.com"
+
+
+def build_csr(subject: str, san: str | None = None, ca: bool | None = None) -> x509.CertificateSigningRequest:
+    key = ec.generate_private_key(ec.SECP256R1())
+    builder = x509.CertificateSigningRequestBuilder()
+    builder = builder.subject_name(x509.Name([x509.NameAttribute(NameOID.COMMON_NAME, subject)]))
+    if san is not None:
+        builder = builder.add_extension(x509.SubjectAlternativeName([x509.DNSName(san)]), critical=False)
+    if ca is not None:
+        builder = builder.add_extension(
+            x509.BasicConstraints(ca=ca, path_length=None),
+            critical=True,
+        )
+    return builder.sign(key, hashes.SHA256())
+
+
+def test_x509_csr_name_correct() -> None:
+    csr = build_csr(subject=NAME_1, san=NAME_1)
+    verify_x509_csr(name=NAME_1, csr=csr)
+
+
+def test_x509_csr_subject_mismatch() -> None:
+    csr = build_csr(subject=NAME_2, san=NAME_1)
+    with pytest.raises(ValueError):
+        verify_x509_csr(name=NAME_1, csr=csr)
+
+
+def test_x509_csr_san_mismatch() -> None:
+    csr = build_csr(subject=NAME_1, san=NAME_2)
+    with pytest.raises(ValueError):
+        verify_x509_csr(name=NAME_1, csr=csr)
+
+
+def test_x509_csr_san_missing() -> None:
+    csr = build_csr(subject=NAME_1)
+    with pytest.raises(ValueError):
+        verify_x509_csr(name=NAME_1, csr=csr)
+
+
+def test_x509_csr_is_ca() -> None:
+    csr = build_csr(subject=NAME_1, san=NAME_1, ca=True)
+    with pytest.raises(ValueError):
+        verify_x509_csr(name=NAME_1, csr=csr)
