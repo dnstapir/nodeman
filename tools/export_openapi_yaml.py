3eceb9fa4afad06085e15d6b4710aa4e00d36043
Jakob Schlyter <jakob@kirei.se>
2024-11-28 11:42:12 +0100

    use test setup for openapi export

--- tools/export_openapi_yaml.py
+++ tools/export_openapi_yaml.py
@@ -1,11 +1,15 @@
 from fastapi.testclient import TestClient
+from pydantic_settings import SettingsConfigDict
 
 from nodeman.server import NodemanServer
 from nodeman.settings import Settings
+
+Settings.model_config = SettingsConfigDict(toml_file="tests/test.toml")
 
 settings = Settings()
 app = NodemanServer(settings)
 
 client = TestClient(app)
 response = client.get("/openapi.yaml")
+response.raise_for_status()
 print(response.text)
