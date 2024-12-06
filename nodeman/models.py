33c3f97cea356126855bc5ddf18b2e27a823eac7
Jakob Schlyter <jakob@kirei.se>
2024-12-06 08:12:48 +0100

    implement proper JWK schema (for OpenAPI)

--- nodeman/models.py
+++ nodeman/models.py
@@ -4,8 +4,10 @@ from typing import Self
 
 from cryptography.x509 import load_pem_x509_certificates
 from pydantic import BaseModel, Field, field_validator
+from pydantic.types import AwareDatetime
 
 from .db_models import TapirNode
+from .jose import PublicJwk, public_key_factory
 from .settings import MqttUrl
 
 MAX_REQUEST_AGE = 300
@@ -24,23 +26,8 @@ class PublicKeyFormat(StrEnum):
         raise ValueError(f"Unsupported format. Acceptable formats: {[f.value for f in cls]}")
 
 
-class PublicJwk(BaseModel):
-    kty: str
-
-    # for EC and ED
-    crv: str | None = None
-    x: str | None = None
-
-    # for EC
-    y: str | None = None
-
-    # for RSA
-    n: str | None = None
-    e: str | None = None
-
-
 class NodeRequest(BaseModel):
-    timestamp: datetime = Field(title="Timestamp")
+    timestamp: AwareDatetime = Field(title="Timestamp")
     x509_csr: str = Field(title="X.509 Client Certificate Bundle")
 
     @field_validator("timestamp")
@@ -61,13 +48,13 @@ class RenewalRequest(NodeRequest):
 class NodeInformation(BaseModel):
     name: str = Field(title="Node name")
     public_key: PublicJwk | None = Field(title="Public key")
-    activated: datetime | None = Field(title="Activated")
+    activated: AwareDatetime | None = Field(title="Activated")
 
     @classmethod
     def from_db_model(cls, node: TapirNode):
         return cls(
             name=node.name,
-            public_key=PublicJwk(**node.public_key) if node.public_key else None,
+            public_key=public_key_factory(node.public_key) if node.public_key else None,
             activated=node.activated,
         )
 
