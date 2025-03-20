553f4e55125923216f7ead45cb627eb72d107568
Jakob Schlyter <jakob@kirei.se>
2025-03-20 16:30:10 +0100

    Return 400 on invalid CSR

--- nodeman/nodes.py
+++ nodeman/nodes.py
@@ -396,7 +396,10 @@ async def enroll_node(
         node.thumbprint = public_key.thumbprint()
 
     # Verify X.509 CSR and issue certificate
-    x509_csr = x509.load_pem_x509_csr(message.x509_csr.encode())
+    try:
+        x509_csr = x509.load_pem_x509_csr(message.x509_csr.encode())
+    except ValueError as exc:
+        raise HTTPException(status.HTTP_400_BAD_REQUEST, detail="Invalid CSR") from exc
     with tracer.start_as_current_span("issue_certificate"):
         node_certificate = process_csr_request(csr=x509_csr, name=name, request=request)
 
@@ -463,7 +466,10 @@ async def renew_node(
             raise HTTPException(status.HTTP_400_BAD_REQUEST) from exc
 
     # Verify X.509 CSR and issue certificate
-    x509_csr = x509.load_pem_x509_csr(message.x509_csr.encode())
+    try:
+        x509_csr = x509.load_pem_x509_csr(message.x509_csr.encode())
+    except ValueError as exc:
+        raise HTTPException(status.HTTP_400_BAD_REQUEST, detail="Invalid CSR") from exc
     with tracer.start_as_current_span("issue_certificate"):
         res = process_csr_request(csr=x509_csr, name=name, request=request)
 
