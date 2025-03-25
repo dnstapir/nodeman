2896606f9c9eb011e467b7d355ccc4177826a064
Jakob Schlyter <jakob@kirei.se>
2025-03-25 16:51:06 +0100

    Limit concurrent passwords verifications

--- nodeman/authn.py
+++ nodeman/authn.py
@@ -1,23 +1,29 @@
+import asyncio
 import logging
 from typing import Annotated
 
 from fastapi import Depends, HTTPException, Request, status
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
+PASSWORD_VERIFIERS_LIMIT = 2
+
 logger = logging.getLogger(__name__)
 
 security = HTTPBasic()
 
+verify_password_semaphore = asyncio.Semaphore(PASSWORD_VERIFIERS_LIMIT)
 
-def get_current_username(
+
+async def get_current_username(
     request: Request,
     credentials: Annotated[HTTPBasicCredentials, Depends(security)],
 ):
     if user := request.app.users.get(credentials.username):
-        if user.verify_password(credentials.password):
-            return credentials.username
-        else:
-            logger.warning("Invalid password for user %s", credentials.username)
+        async with verify_password_semaphore:
+            if user.verify_password(credentials.password):
+                return credentials.username
+            else:
+                logger.warning("Invalid password for user %s", credentials.username)
     else:
         logger.warning("Unknown user %s", credentials.username)
 
