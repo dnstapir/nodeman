d508843d8e10b2a94c25d3e224a10f74ae330620
Jakob Schlyter <jakob@kirei.se>
2025-11-19 08:00:10 +0100

    Fix healthcheck ACL (#145)
    * Move healthcheck endpoint
    
    * responses not needed
    
    * Do not skip healthcheck ACL on invalid IP address
    
    * Add warning for missing client ip address

--- nodeman/healthcheck.py
+++ nodeman/healthcheck.py
@@ -1,10 +1,12 @@
 import ipaddress
-from contextlib import suppress
+import logging
 
 from fastapi import APIRouter, HTTPException, Request, status
 
 from .db_models import TapirCertificate, TapirNode
 from .models import HealthcheckResult
+
+logger = logging.getLogger(__name__)
 
 router = APIRouter()
 
@@ -16,19 +18,28 @@ router = APIRouter()
 def healthcheck(
     request: Request,
 ) -> HealthcheckResult:
-    """Perform healthcheck with database and S3 access"""
+    """Perform health check with database and S3 access"""
 
     if request.client and request.client.host:
-        with suppress(ValueError):
+        try:
             client_address = ipaddress.ip_address(request.client.host)
-            for address in request.app.settings.http.healthcheck_hosts:
-                if client_address in address:
-                    break
-            else:
-                raise HTTPException(
-                    status_code=status.HTTP_403_FORBIDDEN,
-                    detail="You are not my physician",
-                )
+        except ValueError as exc:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail=f"Invalid client IP address: {request.client.host}",
+            ) from exc
+
+        for address in request.app.settings.http.healthcheck_hosts:
+            if client_address in address:
+                break
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="You are not my physician",
+            )
+    else:
+        # Always allow health check if no client IP is provided
+        logger.warning("No client IP provided in healthcheck request")
 
     try:
         node_count = TapirNode.objects().count()
